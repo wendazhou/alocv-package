217f2e50f2e4239808e58c4c30bf14c7493aa83c
Wenda Zhou <wz2335@columbia.edu>
2018-04-15 17:11:03 -0400

    add more functionality for lasso ALO

--- python-package/alocv/_lasso.py
+++ python-package/alocv/_lasso.py
@@ -5,6 +5,83 @@
 import numpy as np
 from scipy.linalg import cholesky, solve_triangular, solve
 
+from ._cholesky_c import cholappend
+
+
+def compute_alo_lasso_reference(X, y, beta_hats):
+    """ Compute ALO approximation of error for lasso.
+
+    Parameters
+    ----------
+    X: the sensing matrix
+    y: the observations
+    beta_hats: A matrix [num_observations, num_alphas] of fits for a number
+        of tuning along the path.
+
+    Returns
+    -------
+    alo_values: The ALO mean-squared error for each tuning.
+    """
+    residuals = y - np.dot(X, beta_hats)
+
+    alo_values = np.empty(beta_hats.shape[1])
+
+    for i in range(beta_hats.shape[1]):
+        E = np.abs(beta_hats[:, i]) > 1e-5
+        h = compute_h_lasso(X, E)
+        alo_values[i] = np.mean(np.square(residuals / (1 - h)))
+
+    return alo_values
+
+
+def compute_cholesky(X, index):
+    S = np.dot(X[:, index].T, X[:, index])
+    return cholesky(S, lower=True, overwrite_a=True, check_finite=False)
+
+
+def update_cholesky(X, L, index, index_new):
+    index_added = set(index_new) - set(index)
+
+    index = list(index)
+
+    for i in index_added:
+        index.append(i)
+        L = cholappend(L, np.dot(X[:, index].T, X[:, i]), np.dot(X[:, i].T, X[:, i]))
+
+    return L, index
+
+
+def compute_leverage_cholesky(X, L, index):
+    W = X[:, index]
+    return np.sum(solve_triangular(L, W.T, check_finite=False) ** 2, axis=0)
+
+
+def compute_alo_lasso(X, y, beta_hats):
+    residuals = y - np.dot(X, beta_hats)
+    alo_values = np.empty(beta_hats.shape[1])
+
+    cholesky_current = None
+    active_index = None
+
+    for i in range(beta_hats.shape[1]):
+        E = np.abs(beta_hats[:, i]) > 1e-5
+        num_active = np.sum(E)
+
+        if num_active == 0:
+            cholesky_current = None
+            active_index = []
+            continue
+
+        if cholesky_current is None:
+            active_index = np.flatnonzero(E)
+            W = X[:, E]
+            S = np.dot(W.T, W)
+            cholesky_current = cholesky(S, lower=True, overwrite_a=True, check_finite=False)
+        else:
+            current_index = np.flatnonzero(E)
+            new_index = np.array(set(current_index) - set(active_index))
+    pass
+
 
 def compute_h_lasso(X, E):
     """ Compute leverage for Lasso estimator.
@@ -21,6 +98,13 @@ def compute_h_lasso(X, E):
     -------
     h: a vector representing the leverage values for each observation
     """
+    num_active = np.sum(E)
+
+    if num_active == 0:
+        return np.zeros(X.shape[0])
+    elif num_active >= X.shape[1]:
+        return np.ones(X.shape[0])
+
     W = X[:, E]
     S = np.dot(W.T, W)
     K = cholesky(S, overwrite_a=True, check_finite=False)
--- python-package/tests/test_lasso.py
+++ python-package/tests/test_lasso.py
@@ -1,10 +1,11 @@
 from alocv import _lasso as lasso
 
 import numpy as np
+import sklearn.linear_model as linear_model
 
 
 def make_test_case(n, p, k):
-    X = np.random.randn(n, p)
+    X = np.random.randn(p, n).T
     beta = np.random.randn(p) * np.concatenate((np.ones(k), np.zeros(p - k)))
     np.random.shuffle(beta)
     y = np.dot(X, beta) + np.random.randn(n) * 0.1
@@ -21,3 +22,57 @@ def test_compute_h():
     h_naive = lasso.compute_h_lasso_naive(X, E)
 
     assert np.linalg.norm(h_computed - h_naive) < 0.05
+
+
+def test_compute_alo_path():
+    X, y = make_test_case(100, 20, 10)
+
+    alphas, beta_hats, _ = linear_model.lasso_path(X, y)
+    alo = lasso.compute_alo_lasso_reference(X, y, beta_hats)
+
+    assert len(alo) == len(alphas)
+
+
+def test_update_cholesky_single():
+    X, y = make_test_case(50, 5, 2)
+
+    E = np.array([True, False, False, False, False])
+    E_new = np.array([True, False, True, False, False])
+
+    L = lasso.compute_cholesky(X, E)
+    L_new, index_new = lasso.update_cholesky(X, L, np.flatnonzero(E), np.flatnonzero(E_new))
+    L_new_truth = lasso.compute_cholesky(X, E_new)
+
+    assert np.linalg.norm(
+        lasso.compute_leverage_cholesky(X, L_new, index_new)
+        - lasso.compute_leverage_cholesky(X, L_new_truth, E_new)) < 0.01
+
+
+def test_update_cholesky_no_order():
+    X, y = make_test_case(50, 5, 2)
+
+    E = np.array([True, False, True, False, False])
+    E_new = np.array([True, True, True, False, False])
+
+    L = lasso.compute_cholesky(X, E)
+    L_new, index_new = lasso.update_cholesky(X, L, np.flatnonzero(E), np.flatnonzero(E_new))
+    L_new_truth = lasso.compute_cholesky(X, E_new)
+
+    assert np.linalg.norm(
+        lasso.compute_leverage_cholesky(X, L_new, index_new)
+        - lasso.compute_leverage_cholesky(X, L_new_truth, E_new)) < 0.01
+
+
+def test_update_cholesky_multiple():
+    X, y = make_test_case(50, 5, 2)
+
+    E = np.array([True, False, True, False, False])
+    E_new = np.array([True, True, True, True, False])
+
+    L = lasso.compute_cholesky(X, E)
+    L_new, index_new = lasso.update_cholesky(X, L, np.flatnonzero(E), np.flatnonzero(E_new))
+    L_new_truth = lasso.compute_cholesky(X, E_new)
+
+    assert np.linalg.norm(
+        lasso.compute_leverage_cholesky(X, L_new, index_new)
+        - lasso.compute_leverage_cholesky(X, L_new_truth, E_new)) < 0.01
