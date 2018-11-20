dec4b76d586cf92aadca98caf537e5f248de4d8f
Wenda Zhou <zhouwenda@outlook.com>
2018-11-20 00:07:29 -0500

    switch to MKL single library

--- C/tests/test_alo_svm.cpp
+++ C/tests/test_alo_svm.cpp
@@ -6,14 +6,15 @@
 #include <algorithm>
 #include <cmath>
 
+
 TEST_CASE("ALO SVM Correct", "[SVM]") {
 	// Includes a number of definitions
 #include "examples/svm_example.in"
 
     double alo_hinge;
     double* leverage = static_cast<double*>(blas_malloc(16, n * sizeof(double)));
-	auto k_copy = blas_unique_alloc<double>(16, std::size(K));
-	std::copy(K, K + std::size(K), k_copy.get());
+    auto k_copy = blas_unique_alloc<double>(16, n * n);
+    std::copy(K, K + n * n, k_copy.get());
 
     svm_compute_alo(n, k_copy.get(), y, alpha, rho, lambda, 1e-5, leverage, &alo_hinge);
 
